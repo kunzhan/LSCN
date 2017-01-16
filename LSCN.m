f44f9504eb10a458507d1bbb154be899c891ad3f
绽琨 <kunzhan@users.noreply.github.com>
2017-01-16 09:45:32 +0800

    Update LSCN.m

--- LSCN.m
+++ LSCN.m
@@ -27,49 +27,6 @@ function I1 = LSCN(Im)
     I1 = GrayStretch(uint8(255*mat2gray(L)),0.98);
     I1 = I1(2:end-1,2:end-1);
 end
-function M = imsmooth(S, R, k)
-    [hei, wid] = size(S);
-    N = boxfilter(ones(hei, wid), R); 
-    mu_w = boxfilter(S, R) ./ N;
-    sigma_w = boxfilter(S.*S, R) ./ N;
-    var_I = sigma_w - mu_w .* mu_w;
-    k = var_I ./ (var_I + k); 
-    M = mu_w + k.*(S - mu_w);
-end
-function GS = GrayStretch(I,Per)
-    [m,M] = FindingMm(I,Per);
-    GS = uint8((double(I)-m)./(M-m)*255);
-end
-function [minI,MaxI] = FindingMm(I,Per)
-    h = imhist(I);
-    All = sum(h);
-    ph = h ./ All;
-    mth_ceiling = BoundFinding(ph,Per);
-    Mph=fliplr(ph')';
-    Mth_floor = BoundFinding(Mph,Per);
-    Mth_floor = 256 - Mth_floor + 1;
-    ConstraintJudge = @(x,y) sum(h(x:y))/All >= Per;
-    Difference = zeros(256,256) + inf;
-    for m = mth_ceiling:-1:1
-        for M = Mth_floor:256
-            if (h(m) > 0 )&&(h(M) > 0)
-                if ConstraintJudge(m,M)
-                    Difference(m,M) = M - m;
-                end
-            end
-        end
-    end
-    minD = min(Difference(:));
-    [m, M] = find(Difference==minD);
-    minI = m(1) - 1;
-    MaxI = M(1) - 1;
-end
-function m_ceiling = BoundFinding(ph,Per)
-    cumP = cumsum(ph);
-    n = 1;
-    residualP = 1-Per;
-    while cumP(n) < residualP
-        n = n + 1;
-    end
-    m_ceiling = n;
-end
+
+
+    
