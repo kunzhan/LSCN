40c779fa8d2764142ab1df2d903f4857a9a26603
绽琨 <kunzhan@users.noreply.github.com>
2017-04-01 17:49:24 +0800

    Update imsmooth.m

--- tools/imsmooth.m
+++ tools/imsmooth.m
@@ -1,9 +1,9 @@
-function M = imsmooth(S, R, k)
+function M = imsmooth(S, R, lambda)
 [hei, wid] = size(S);
 N = boxfilter(ones(hei, wid), R); 
 mu_w = boxfilter(S, R) ./ N;
 sigma_w = boxfilter(S.*S, R) ./ N;
 var_I = sigma_w - mu_w .* mu_w;
-k = var_I ./ (var_I + k); 
+k = var_I ./ (var_I + lambda); 
 M = mu_w + k.*(S - mu_w);
 end
